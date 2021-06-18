2c441553406f962a4dae7a68eef60cc41498ff43
Sz0n <kamilrsw@gmail.com>
2021-06-18 18:06:39 +0200

    added search and order function for specific endpoints

--- manager/urls.py
+++ manager/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 
 urlpatterns = [
-    path('artists/', views.ArtistList.as_view()),
-    path('artists/<int:pk>', views.ArtistDetail.as_view()),
-    path('events/', views.EventList.as_view()),
-    path('events/<int:pk>', views.EventDetail.as_view()),
-    path('rel-artists/<int:pk>', views.ArtistSameEventsList.as_view())
+    path('artists/', views.ArtistList.as_view(), name="artists_list"),
+    path('artists/<int:pk>', views.ArtistDetail.as_view(), name="artist_detail"),
+    path('events/', views.EventList.as_view(), name="events_list"),
+    path('events/<int:pk>', views.EventDetail.as_view(), name="event_detail"),
+    path('rel-artists/<int:pk>', views.ArtistSameEventsList.as_view(), name="related_artists")
 ]
--- manager/views.py
+++ manager/views.py
@@ -1,5 +1,5 @@
 from django.http import Http404
-from rest_framework import generics
+from rest_framework import generics, status, filters
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Artist, Event
@@ -10,59 +10,50 @@ class ArtistList(generics.ListCreateAPIView):
     serializer_class = ArtistSerializer
 
     def get_queryset(self):
-        artist = Artist.objects.all()
-        return artist
+        queryset = Artist.objects.all()
+        return queryset
 
     def create(self, request, *args, **kwargs):
-        data = request.data
+        serializer = ArtistSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-        new_artist = Artist.objects.create(name=data['name'], genre=data['genre'])
-
-        new_artist.save()
-
-        serializer = ArtistSerializer(new_artist)
-
-        return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class ArtistDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = ArtistSerializer
 
     def get_queryset(self):
-        artist = Artist.objects.all()
-        return artist
+        queryset = Artist.objects.all()
+        return queryset
 
 
 class EventList(generics.ListCreateAPIView):
     serializer_class = EventSerializer
+    filter_backends = (filters.SearchFilter, filters.OrderingFilter)
+    search_fields = ("name",)
 
     def get_queryset(self):
-        event = Event.objects.all()
-        return event
+        queryset = Event.objects.all()
+        return queryset
 
     def create(self, request, *args, **kwargs):
-        data = request.data
+        serializer = EventSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-        new_event = Event.objects.create(name=data['name'], description=data["description"], location=data["location"],
-                                         latitude=data["latitude"], longitude=data["longitude"])
-
-        new_event.save()
-
-        for artist in data['artists']:
-            artist_obj = Artist.objects.get(name=artist['name'])
-            new_event.artists.add(artist_obj)
-
-        serializer = EventSerializer(new_event)
-
-        return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class EventDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = EventSerializer
 
     def get_queryset(self):
-        event = Event.objects.all()
-        return event
+        queryset = Event.objects.all()
+        return queryset
 
 
 class ArtistSameEventsList(APIView):
@@ -80,5 +71,10 @@ class ArtistSameEventsList(APIView):
         for event in related_events:
             related_artists |= event.artists.all()
 
-        serializer = RelatedArtistSerializer(related_artists.distinct(), many=True)
-        return Response(serializer.data)
+        if request.GET.get("search") is not None:
+            related_artists = related_artists.filter(name=request.GET.get("search"))
+
+        serializer = RelatedArtistSerializer(
+            related_artists.distinct().order_by(request.GET.get("ordering", "id")), many=True)
+
+        return Response(serializer.data, status.HTTP_200_OK)
